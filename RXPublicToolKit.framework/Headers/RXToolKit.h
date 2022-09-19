b27171831ecbee12a7777368eb1eae1c1b76f14e
chenhan <chenhan@jixiang.cn>
2022-09-19 16:41:01 +0800

    1.0.0 获取ua

---
+++ RXPublicToolKit.framework/Headers/RXToolKit.h
@@ -0,0 +1,26 @@
+//
+//  RXToolKit.h
+//  RXPublicToolKit
+//
+//  Created by 陈汉 on 2022/9/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RXToolKit : NSObject
+
+/**
+ * 获取SDK实例（单例）
+ */
++ (instancetype)sharedSDK;
+
+/**
+ * 获取userAgent
+ */
+- (NSString *)getUserAgent;
+
+@end
+
+NS_ASSUME_NONNULL_END
